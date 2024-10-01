df6c7244ad77b4a170e4def84d412ddbce80713d
cacteyy <m.wawrzyniaczek@gmail.com>
2024-10-01 21:52:50 +0200

    updated gitignore

--- qr_code_generator/generator.cpp
+++ qr_code_generator/generator.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <fstream>
 
-bool isBitOn(int value, int bit){
-    if(bit < 0 or bit >= 8) return false;
-
-    int mask = 1<<bit;
-    return (bool)(value&mask);
-}
-
 int main(){
     std::string data_to_encode;
     std::cin>>data_to_encode;
 
-    unsigned int bytes_of_data = data_to_encode.size();
+    unsigned short bytes_of_data = data_to_encode.size();
 
     for(char current_char : data_to_encode){
         for(int i = 7; i>=0; i--){
